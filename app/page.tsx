0b18f464aabe8980a71e2b08ea7b777d2754e973
thomasmeylaers <thomas.meylaers@student.kuleuven.be>
2023-02-21 16:46:23 +0100

    build: testing deployment to netlify

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import DogArray from './components/DogArray';
 export default function Home() {
   return (
     <main className="w-[75%] mx-auto mt-[5rem]">
+      {/* @ts-expect-error Server Component */}
       <DogArray amount={3} />
     </main>
   );
